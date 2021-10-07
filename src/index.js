cc2cc4e0c59bcd096a579ed02baddb2b0c6809f5
Joanna-Golofit <sunderlajsa@gmail.com>
2021-10-07 13:41:22 +0200

    złapane cos z inputa

--- src/index.js
+++ src/index.js
@@ -1,23 +1,50 @@
 import axios from 'axios';
+// robilam simplelightbox przez npm.. to nie wiem co z tym css - chyba nie trzeba
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const SearchForm = document.querySelector("form#search-form");
+const inputSearch = document.querySelector("input[name='searchQuery']");
+const gallery = document.querySelector('div.gallery');
+const btnMore = document.querySelector('button.load-more');
+let perPage = 40;
+let page = 1;
+
+/////////////// łapie search value /////////////:
+const showData = e => {
+  e.preventDefault();
+  let searchValue = inputSearch.value;
+  console.log('showData, searchValue', searchValue);
+};
+// inputSearch.addEventListener('click', showData );
+
+SearchForm.addEventListener('submit', showData);
+/////////////////////////////////////////////
+
+btnMore.style.display = 'none';
 
 const q = "cat dog";
 const API_KEY = "23726584-b0725e8cc2245e4091c11b21f";
-let page = 1;
 
-axios
+// zeby przerzucic tu axios:
+const fetchPicts = searchValue => {
+
+}
+
+  axios
   .get(
     `https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`,
   )
   .then(response => {
-    console.log('response', response);
-    console.log('q', q);
+    // console.log('searchValue:', searchValue);
+    console.log('q:', q);
     console.log('API_KEY', API_KEY);
+    console.log('response:', response);
     console.log('response.data', response.data);
     console.log('response.data.hits', response.data.hits);
     console.log('response.data.hits[0]', response.data.hits[0]);
     console.log('response.data.hits[1].pageURL', response.data.hits[1].pageURL);
     console.log('response.data.hits[2].pageURL', response.data.hits[2].pageURL);
-    page++;
+    // page++;
     console.log('page', page);
   })
   .catch(err => console.log("Caught error:", err));;
