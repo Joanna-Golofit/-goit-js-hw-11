8cfa04b7264cb812b1f4daea775848d487d952ba
Joanna-Golofit <sunderlajsa@gmail.com>
2021-10-09 11:48:44 +0200

    znowu sie zdjecia wyswietlaja, async/await

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// robilam simplelightbox przez npm.. to nie wiem co z tym css - chyba nie trzeba
+// robilam simplelightbox przez npm.. , odinstalowalam, probuje przez importy.. cos nie dziala ciagle i sie nie wyswietla
 // Opisany w dokumentacji
 import SimpleLightbox from 'simplelightbox';
 // Dodatkowy import stylów
@@ -12,31 +12,56 @@ const btnMore = document.querySelector('button.load-more');
 let perPage = 40;
 let page = 1;
 
-const showPictures = e => {
+// najpierw deklaracja asynchronicznej funkcji fetchPictures:
+async function fetchPictures(inputSearchValue) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=10`,
+    );
+    // console.log('deklaracja fetchPictures response:', response); // dziala
+    return response.data;
+  } catch (error) {
+    console.log('deklaracja fetchPictures ,async/await, error:', error.message);
+  }
+}
+
+//  deklaracja asynchronicznej funkcji showPictures:
+
+async function showPictures(e) {
   // zapobiega domyslnemu przeladowaniu strony po wyslaniu formularza:
   e.preventDefault();
   // łapie inputSearchValue :
   let inputSearchValue = inputSearch.value;
   //wyswietla to, co zlapalo:
   console.log('showData, inputSearchValue:', inputSearchValue);
-
-  /////////////// wyszukuje w API po inputSearchValue i dodatkowych kryteriach /////////////:
-
-  axios
-    .get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=10`,
-    )
-    .then(response => {
-      console.log('inputSearchValue:', inputSearchValue);
-      console.log('response.data', response.data.hits[0]);
-      // page++;
-      console.log('page', page);
-      renderGallery(response);
+  // wywołanie funkcji:
+  fetchPictures(inputSearchValue)
+    .then(respData => {
+      console.log('wywolanie fetchPictures respData', respData); // dziala
+      console.log('pod fetchPictures respData.hits', respData.hits);
+      renderGallery(respData);
       btnMore.style.display = 'block';
     })
-    .catch(err => console.log('Caught error:', err));
+    .catch(error => console.log(error));
+
+
+  /////////////// wyszukuje w API po inputSearchValue i dodatkowych kryteriach /////////////:
+  // axios
+  //   .get(
+  //     `https://pixabay.com/api/?key=${API_KEY}&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=10`,
+  //   )
+  //   .then(response => {
+  //     console.log('inputSearchValue:', inputSearchValue);
+  //     console.log('response.data', response.data.hits[0]);
+  //     // page++;
+  //     console.log('page', page);
+  //     renderGallery(response);
+  //     btnMore.style.display = 'block';
+  //   })
+  //   .catch(err => console.log('Caught error:', err));
+  //////////////////////////////////////////
 };
-// inputSearch.addEventListener('click', showData );
+
 
 SearchForm.addEventListener('submit', showPictures);
 /////////////////////////////////////////////
@@ -46,17 +71,14 @@ btnMore.style.display = 'none';
 const q = 'cat dog';
 const API_KEY = '23726584-b0725e8cc2245e4091c11b21f';
 
-// zeby przerzucic tu axios:
-// const fetchPicts = inputSearchValue => {
 
-// }
-
-const renderGallery = response => {
-  const markup = response.data.hits
+// deklaracja funkcji renderGallery do tworzenia znacznikow galerii w html:
+const renderGallery = respData => {
+  const markup = respData.hits
     .map(
       hit =>
         `<div class="photo-card">
-      <a class="gallery__item" href=${hit.largeImageURL}>
+    <a class="gallery__item" href=${hit.largeImageURL}>
       <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
       </a>
       <div class="info">
@@ -80,14 +102,26 @@ const renderGallery = response => {
   gallery.innerHTML = markup;
 };
 
+//powinno odpalic simplelightbox...
 var lightbox = new SimpleLightbox('.gallery a');
 
 // let lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
+  //   captionsData: 'alt',
 //   captionDelay: 250,
 // });
 
 //*********************************************************************
+
+/////////////////// Want to use async/await? Add the `async` keyword to your outer function/method.///////////////
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+//////////////////////////////////////////////////
 
 //////////////////// logi z axios.then////////////////////
 // console.log('API_KEY', API_KEY); // dziala
@@ -125,18 +159,9 @@ var lightbox = new SimpleLightbox('.gallery a');
 // console.log('oy');
 //////////////////////////////////////////////////
 
-/////////////////////// Want to use async/await? Add the `async` keyword to your outer function/method./////////////////////
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-//////////////////////////////////////////////////
 
-///////////////////async/await potrzebny///////////////////////////////
+
+///////////////////async/await fetch potrzebny///////////////////////////////
 //deklaracja:
 // const fetchPlaceholder = async () => {
 //   const postsJson = await fetch(
@@ -156,3 +181,4 @@ var lightbox = new SimpleLightbox('.gallery a');
 // fetchPlaceholder()
 //   .then(d => console.log(d))
 //   .catch(err => console.log(err));
+//////////////////////////////////////////////////
